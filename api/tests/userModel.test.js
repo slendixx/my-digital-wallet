a6a7ebfb07ce215ed6e4a941d88ef9d687beaa45
Esteban <esteban321duran@gmail.com>
2022-08-03 12:27:09 -0300

    Revert "configured environment for user model tests"
    This reverts commit 4bb47e98d2e37cc75a50ad215dcdb438cc496d30.
    db queries no longer use a single connection from the dbConnectivity
    module. These configurations are no longer necessary.

--- api/tests/userModel.test.js
+++
@@ -1,5 +0,0 @@
-const db = require("../src/model/dbConnectivity");
-
-beforeAll(async () => await db.createConnection());
-afterEach(async () => await db.clear());
-afterAll(async () => await db.disconnect());
