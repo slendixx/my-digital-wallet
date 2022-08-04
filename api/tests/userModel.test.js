2bb0777734f73a4a7243aafb1f7b4e28fdff2032
Esteban <esteban321duran@gmail.com>
2022-08-03 21:25:33 -0300

    refactored to remove duplication

--- api/tests/userModel.test.js
+++ api/tests/userModel.test.js
@@ -1,34 +1,30 @@
 const model = require("../src/model/user");
 const { clear } = require("../src/model/dbConnectivity");
+
 afterEach(async () => await clear());
+
+const userData = {
+  email: "esteban@abc.com",
+  firstName: "esteban",
+  lastName: "duran",
+  password: "esteban12345",
+};
 describe("User Model", () => {
-    it("should create user and return it's data", () => {
-        const userData = {
-            email: "esteban@abc.com",
-            firstName: "esteban",
-            lastName: "duran",
-            password: "esteban12345",
-        };
-        return model.create(userData).then((createdUser) => {
-            expect(createdUser.id).toBeDefined();
-            expect(createdUser.email).toBe(userData.email);
-            expect(createdUser.first_name).toBe(userData.firstName);
-            expect(createdUser.last_name).toBe(userData.lastName);
-            expect(createdUser.balance).toBe(0);
-        });
+  it("should create user and return it's data", () => {
+    return model.create(userData).then((createdUser) => {
+      expect(createdUser.id).toBeDefined();
+      expect(createdUser.email).toBe(userData.email);
+      expect(createdUser.first_name).toBe(userData.firstName);
+      expect(createdUser.last_name).toBe(userData.lastName);
+      expect(createdUser.balance).toBe(0);
     });
+  });
 
-    it("should get user by it's id", async () => {
-        const newUserData = {
-            email: "esteban@abc.com",
-            firstName: "esteban",
-            lastName: "duran",
-            password: "esteban12345",
-        };
-        const createdUser = await model.create(newUserData);
+  it("should get user by it's id", async () => {
+    const createdUser = await model.create(userData);
 
-        return model.getById(createdUser.id).then((userData) => {
-            expect(userData).toEqual(createdUser);
-        });
+    return model.getById(createdUser.id).then((userData) => {
+      expect(userData).toEqual(createdUser);
     });
+  });
 });
