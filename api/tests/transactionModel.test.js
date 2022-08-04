ae3d169d5b0d677d1f70543ececbe88fb1afe3be
Esteban <esteban321duran@gmail.com>
2022-08-04 00:31:19 -0300

    refactored test data to test filtered queries
    on module tests/transactionModel

--- api/tests/transactionModel.test.js
+++ api/tests/transactionModel.test.js
@@ -10,45 +10,51 @@ const userData = {
   lastName: "duran",
   password: "esteban12345",
 };
-const transactionData = {
+const transactionData1 = {
   amount: 100,
   type: "incoming",
   category: "other",
+  description: "",
+};
+const transactionData2 = {
+  amount: 50,
+  type: "outgoing",
+  category: "taxes",
   description: "",
 };
 
 describe("Transaction Model", () => {
   it("should create a transaction", async () => {
     const createdUserData = await userModel.create(userData);
-    transactionData.user_id = createdUserData.id;
+    transactionData1.user_id = createdUserData.id;
 
-    const createdTransaction = await model.create(transactionData);
+    const createdTransaction = await model.create(transactionData1);
     expect(createdTransaction.id).toBeDefined();
-    expect(createdTransaction.amount).toBe(transactionData.amount);
-    expect(createdTransaction.type).toBe(transactionData.type);
-    expect(createdTransaction.category).toBe(transactionData.category);
-    expect(createdTransaction.user_id).toBe(transactionData.user_id);
+    expect(createdTransaction.amount).toBe(transactionData1.amount);
+    expect(createdTransaction.type).toBe(transactionData1.type);
+    expect(createdTransaction.category).toBe(transactionData1.category);
+    expect(createdTransaction.user_id).toBe(transactionData1.user_id);
     expect(createdTransaction.date).toBeDefined();
 
-    const user = await userModel.getById(transactionData.user_id);
+    const user = await userModel.getById(transactionData1.user_id);
     expect(user.balance).toBe(100);
   });
 
   it("should delete a transaction by id and decrease user balance by the transaction amount", async () => {
     const createdUserData = await userModel.create(userData);
-    transactionData.user_id = createdUserData.id;
+    transactionData1.user_id = createdUserData.id;
 
-    const createdTransaction = await model.create(transactionData);
+    const createdTransaction = await model.create(transactionData1);
     const message = await model.delete(createdTransaction.id);
     expect(message).toBe("transaction deleted");
-    const user = await userModel.getById(transactionData.user_id);
+    const user = await userModel.getById(transactionData1.user_id);
     expect(user.balance).toBe(0);
   });
 
   it("should update amount, category, description by transaction id", async () => {
     const createdUserData = await userModel.create(userData);
-    transactionData.user_id = createdUserData.id;
-    const createdTransaction = await model.create(transactionData);
+    transactionData1.user_id = createdUserData.id;
+    const createdTransaction = await model.create(transactionData1);
     //user.balance is now 100
     const newData = {
       id: createdTransaction.id,
@@ -74,17 +80,20 @@ describe("Transaction get", () => {
   it("should get all transactions for a user id", async () => {
     //TODO refactor the next 3 lines to remove duplication
     const createdUserData = await userModel.create(userData);
-    transactionData.user_id = createdUserData.id;
+    transactionData1.user_id = createdUserData.id;
+    transactionData2.user_id = createdUserData.id;
     const transactionsData = [
-      await model.create(transactionData),
-      await model.create(transactionData),
+      await model.create(transactionData1),
+      await model.create(transactionData2),
     ];
 
     return model.getByUserId(createdUserData.id).then((transactions) => {
       expect(transactions).toEqual(transactionsData);
     });
   });
-  // it("should get all transactions for a user_id filtered by category", () => {});
+  it("should get all transactions for a user_id filtered by category", () => {
+    expect();
+  });
   // it("should get all transactions for a user_id filtered by type", () => {});
   // it("should get all transactions for a user_id filtered by type & category", () => {});
   // it("should get all transactions for a user_id from least recent to most recent", () => {});
